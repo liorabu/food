2f64b146a7019b3727ac348b7e1dc7a6d10d5087
liorabu <64246054+liorabu@users.noreply.github.com>
2020-12-24 12:04:33 +0200

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL:'https://api.yelp.com/v3/businesses',
     headers:{
-        Authorization:'Bearer r1TARVbUhCQ7GaPH5oq7QOoHzE4b8B3pcTXt6d2H3MSiFs5aSjgvUqOYQ92pBq6En_nLOEnnSteYRLM3PU_MHbEt30gDP0U3Ygd44RF416-X05BRtmsYmsucpASKXnYx'
+        Authorization:'Bearer PRIVATE'
     }
 });
